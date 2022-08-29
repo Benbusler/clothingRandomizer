075abcb6499e21cfac426a2c97b2db560184f1cf
Benbusler <35205599+Benbusler@users.noreply.github.com>
2022-08-29 00:25:20 -0400

    Closer to done?
    Added the first set of outfits to randomize from, looks good so far

--- main.py
+++ main.py
@@ -5,30 +5,57 @@ from PIL import Image
 pygame.init
 
 #variables
-x = random.randint(2,5)
-text = r'C:\Users\benbu\Documents\GitHub\clothingRandomizer\images\image'
+headx = random.randint(1,2)
+bodyx = random.randint(1,4)
+pantx = random.randint(1,4)
+footx = random.randint(1,4)
+text = r'C:\Users\benbu\Documents\GitHub\clothingRandomizer\images\clothing'
 
-#determining displayed image
-capturedImage = text + str(x) + ".jpg"
-print(capturedImage)
+#determining displayed images
+headChoice = text + r"\head" + str(headx) + ".png"
+bodyChoice = text + r"\body" + str(bodyx) + ".png"
+pantChoice = text + r"\pant" + str(pantx) + ".png"
+footChoice = text + r"\foot" + str(footx) + ".png"
+#print(capturedImage)
 
-img = Image.open(capturedImage)
+#img = Image.open(capturedImage)
 
 # get width and height
-width = img.width
-height = img.height
+#width = img.width
+#height = img.height
 
 #setting up the window stuff
-displayWidth = width
-displayHeight = height
+displayWidth = 400
+displayHeight = 600
 surface = pygame.display.set_mode((displayWidth, 2 * displayHeight))
-pygame.display.set_caption("images")
-displayImage = pygame.image.load(capturedImage)
+pygame.display.set_caption("Fit 4 2 day")
 
+
+
+displayHead = pygame.image.load(headChoice)
+displayBody = pygame.image.load(bodyChoice)
+displayPant = pygame.image.load(pantChoice)
+displayFoot = pygame.image.load(footChoice)
+
+displayHeadO = Image.open(headChoice)
+displayBodyO = Image.open(bodyChoice)
+displayPantO = Image.open(pantChoice)
+displayFootO = Image.open(footChoice)
+
+headHeight = displayHeadO.height
+bodyHeight = displayHeadO.height + displayBodyO.height
+pantHeight = displayHeadO.height + displayBodyO.height + displayPantO.height
+
+headWidth = displayHeadO.width
+bodyWidth = displayBodyO.width
+pantWidth = displayPantO.width
+footWidth = displayFootO.width
 while True:
     surface.fill((255,255,255))
-    surface.blit(displayImage, (0, 0))
-    surface.blit(displayImage, (0, height))
+    surface.blit(displayHead, (displayWidth / 2 - headWidth / 2 , 0))
+    surface.blit(displayBody, (displayWidth / 2 - bodyWidth / 2 , headHeight))
+    surface.blit(displayPant, (displayWidth / 2 - pantWidth / 2 , bodyHeight))
+    surface.blit(displayFoot, (displayWidth / 2 - footWidth / 2, pantHeight))
 
 
     for event in pygame.event.get():
